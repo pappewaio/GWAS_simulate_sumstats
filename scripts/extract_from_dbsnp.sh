8599f6a97650ae85133e161bc0a4be191126c63a
pappewaio <j.r.gadin@gmail.com>
2021-03-22 11:43:16 +0100

    initited the workflow setup

--- scripts/extract_from_dbsnp.sh
+++ scripts/extract_from_dbsnp.sh
@@ -25,6 +25,17 @@ for chr in {1..22} MT X Y; do \
  echo "$chr done ..."; \
 done
 
-
 # Move these chunks into the data folder to be used as example for the merge script
+
+
+# Generate vcf example dataset
+zcat All_20180418.vcf.gz | head -n10000 | grep "#" > All_20180418_example_data.vcf
+for chr in {3..22} MT X Y; do \
+ ( \
+ echo "$chr starting ..."; \
+  zcat All_20180418.vcf.gz | grep -v "#" | awk -vchr=${chr} -vOFS="\t" '{if(chr==$1){print $0}}' | head -n200 >> "All_20180418_example_data.vcf" 
+ echo "$chr done ..."; \
+ ) & \
+done
+
 
--- scripts/merge_simgwas_and_dbsnp_chunks_randomly.sh
+++ scripts/merge_simgwas_and_dbsnp_chunks_randomly.sh
@@ -1,24 +1,34 @@
 # Run using
-./scripts/merge_simgwas_and_dbsnp_chunks_randomly.sh data/dbsnp151/dbsnp_chr22_chunk data/gwas_sim_output/chr22.lin | head
+#./scripts/merge_simgwas_and_dbsnp_chunks_randomly.sh data/dbsnp151/dbsnp_chr22_chunk data/gwas_sim_output/chr22.lin test.out
 
-test_res=$1
-dbsnp_ann=$2
+dbsnp_ann=$1
+test_res=$2
+outfile=$3
+sep="\t"
 
 #Add test_res on the left of dbsnp_ann. The stats will be totally disconnected from the annotation, that is important to remember. This data is only built to create test data for the purpose of software tests.
 
 #check size of both data
-s1=$(wc -l ${test_res})
-s2=$(wc -l ${dbsnp_ann})
-
-if [ ${s1} == ${s2} ]; do
-
-  paste ${test_res} ${dbsnp_ann}
-
+echo "checking size of ${test_res}"
+s1="$(awk 'END{print NR}' ${test_res})"
+echo "checking size of ${dbsnp_ann}"
+s2="$(awk 'END{print NR}' ${dbsnp_ann})"
+if [ "${s1}" -gt "${s2}" ]; then
+  # awk join the two files
+  echo "merging the two files into a file of size: ${s2}"
+  awk -vOFS="${sep}" -vlargerFile=${test_res} '{getline x<largerFile; print $0, x}' ${dbsnp_ann} > ${outfile}
+elif [ "${s1}" -lt "${s2}" ]; then
+  # awk join the two files
+  echo "merging the two files into a file of size: ${s1}"
+  awk -vOFS="${sep}" -vlargerFile=${dbsnp_ann} '{getline x<largerFile; print $0, x}' ${test_res} > ${outfile}
+elif [ "${s1}" -eq "${s2}" ]; then
+  # awk join the two files
+  echo "merging the two files into a file of size: ${s1}"
+  awk -vOFS="${sep}" -vlargerFile=${dbsnp_ann} '{getline x<largerFile; print $0, x}' ${test_res} > ${outfile}
 else
-
-  echo "Not same file sizem, which is required. file1: ${1} versus file2: ${2}"
+  echo "something is wrong with the file size check. file1: ${1}, file2: ${2}"
   exit 1
 
-done
+fi
 
 
